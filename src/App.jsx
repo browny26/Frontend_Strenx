f0e4b37de9a4fdf24074d6cc7757d97a8209f5bc
vladyslav <vlad00anonimous00@gmail.com>
2025-04-01 16:41:21 +0200

    implemented navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,21 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Products from "./pages/Products";
+import { Navbar } from "./components/Navbar";
 
 const App = () => {
-  return (
-    <Router>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
+    return (
+        <Router>
+            <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="*" element={<NotFound />} /> {/* Per gestire 404 */}
-      </Routes>
-    </Router>
-  );
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="*" element={<NotFound />} />{" "}
+                {/* Per gestire 404 */}
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,25 @@
+import { HugeiconsIcon } from "@hugeicons/react";
 import React from "react";
+import { Link } from "react-router-dom";
+import UserIcon from "../assets/icons/user-stroke-rounded.svg";
+import CartIcon from "../assets/icons/shopping-basket-03-stroke-rounded.svg";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+export const Navbar = () => {
+    return (
+        <nav>
+            <h1>
+                STREN<span>X</span>
+            </h1>
+            <div className="links">
+                <Link to="/">Home</Link>
+                <Link to="/products">Products</Link>
+                <Link to="/">About us</Link>
+                <Link to="/">Contacs</Link>
+            </div>
+            <div className="buttons">
+                <img src={UserIcon} alt="User Icon" />
+                <img src={CartIcon} alt="User Icon" />
+            </div>
+        </nav>
+    );
 };
-
-export default Navbar;
