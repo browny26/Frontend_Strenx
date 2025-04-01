016c9fddc5e1efa8809f3ce4a4fdf8037fe3ffaa
vladyslav <vlad00anonimous00@gmail.com>
2025-04-01 17:39:15 +0200

    implementing css for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,32 @@
-import { HugeiconsIcon } from "@hugeicons/react";
 import React from "react";
 import { Link } from "react-router-dom";
 import UserIcon from "../assets/icons/user-stroke-rounded.svg";
 import CartIcon from "../assets/icons/shopping-basket-03-stroke-rounded.svg";
+import "../styles/navbar.css";
 
 export const Navbar = () => {
     return (
         <nav>
-            <h1>
-                STREN<span>X</span>
-            </h1>
-            <div className="links">
-                <Link to="/">Home</Link>
-                <Link to="/products">Products</Link>
-                <Link to="/">About us</Link>
-                <Link to="/">Contacs</Link>
+            <div className="nav-left">
+                <h1>
+                    STREN<span>X</span>
+                </h1>
+                <div className="links">
+                    <Link className="anchor" to="/">
+                        Home
+                    </Link>
+                    <Link className="anchor" to="/products">
+                        Products
+                    </Link>
+                    <Link className="anchor" to="/">
+                        About us
+                    </Link>
+                    <Link className="anchor" to="/">
+                        Contacs
+                    </Link>
+                </div>
             </div>
+
             <div className="buttons">
                 <img src={UserIcon} alt="User Icon" />
                 <img src={CartIcon} alt="User Icon" />
