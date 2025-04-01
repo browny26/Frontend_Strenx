554d8fdbe640c811ec98e539d37e3b9b39996399
LucaMimmo <171295375+LucaMimmo05@users.noreply.github.com>
2025-04-01 23:00:01 +0200

    Implemented register page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Logo from "../components/Logo"; 
+import InputField from "../components/InputField"; 
+import Button from "../components/Button"; 
+import "../styles/register.css";
+const Register = () => {
+
+    const navigate = useNavigate();
+    const handleRegister = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+
+        const req = {
+            email: formData.get("Email"),
+            username: formData.get("Username"),
+            password: formData.get("Password"),
+            role:"user"
+        }
+
+        console.log(req);
+
+        const response = await fetch("http://localhost:3000/api/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(req),
+        });
+
+        if(response.ok) {
+            navigate("/login")
+        }
+        else {
+            console.log("Error during registration");
+        }
+    }
+  return (
+    <div className="content">
+        <div className="left-part">
+          <Logo size={"medium"} />
+          <form method="POST"onSubmit={(e) => handleRegister(e)}  className="register-cont">
+            <h1>Sign Up</h1>
+            <div className="fields-cont">
+              <InputField label={"Email"} type={"email"} />
+              <InputField label={"Username"} type={"text"} />
+              <InputField label={"Password"} type={"password"} />
+            </div>
+            <div className="bottom">
+              <Button type={"submit"} text={"Sign up"} />
+              <p>
+                Already a member?{" "}
+                <Link className="link" to={"/login"}>
+                  Sign In
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
+        <div className="right-part">
+          <div className="right-part-cont">
+            <Logo size={"large"} />
+            <h3>Gear Up. Train Hard. Be Unstoppable!</h3>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Register
